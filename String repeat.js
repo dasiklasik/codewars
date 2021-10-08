2db2ea4de4a627f35ee0ef85b81fe992cfe87ab5
dasiklasik <daryasamsonowicz@gmail.com>
2021-10-08 13:32:35 +0300

    feat: completed task String repeat

---
+++ String repeat.js
@@ -0,0 +1,9 @@
+/*link https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/javascript*/
+
+function repeatStr(n, s) {
+  let res = "";
+  for (let i = 0; i < n; i++) {
+    res += s;
+  }
+  return res;
+}
