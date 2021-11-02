22edc6d093a35aba3e4d8acd4c9986abaffb1cfd
dasiklasik <daryasamsonowicz@gmail.com>
2021-11-02 18:18:35 +0300

    add completed task

---
+++ Basic Mathematical Operations.js
@@ -0,0 +1,14 @@
+/*link https://www.codewars.com/kata/57356c55867b9b7a60000bd7/solutions/javascript*/
+
+function basicOp(operation, value1, value2)
+{
+    if (operation === '+') {
+        return value1 + value2;
+    } else if (operation === '-') {
+        return value1 - value2;
+    } else if (operation === '*') {
+        return value1 * value2;
+    } else if (operation === '/') {
+        return value1 / value2;
+    }
+}
