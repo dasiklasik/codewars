421cab1d9492ccbc39c2dc1306747ecbce0a3b06
dasiklasik <daryasamsonowicz@gmail.com>
2021-11-02 18:22:08 +0300

    add completed task

---
+++ Is n divisible by x and y?.js
@@ -0,0 +1,10 @@
+/*link https://www.codewars.com/kata/5545f109004975ea66000086/solutions/javascript*/
+
+
+function isDivisible(n, x, y) {
+    if (n%x === 0 && n%y === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
