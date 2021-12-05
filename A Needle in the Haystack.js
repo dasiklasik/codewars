a84a33a7ff4fad318269a99e5b7d19780ae6c8ae
dasiklasik <daryasamsonowicz@gmail.com>
2021-12-05 21:17:53 +0300

    add completed task

---
+++ A Needle in the Haystack.js
@@ -0,0 +1,5 @@
+/*link https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/solutions/javascript*/
+
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`
+}
