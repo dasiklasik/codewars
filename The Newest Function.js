7eba02193f908cccf8f06f00d014118f593e93f1
dasiklasik <daryasamsonowicz@gmail.com>
2023-06-08 13:04:25 +0300

    feat: complete task

---
+++ The Newest Function.js
@@ -0,0 +1,8 @@
+/*link https://www.codewars.com/kata/5a2cb53cee1aaef2fa000037/javascript*/
+
+// This function can get pretty new. Probably new enough.
+
+const newFunction = function func() {
+  return func
+}
+
